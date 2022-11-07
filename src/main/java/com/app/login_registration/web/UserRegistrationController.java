affb0f1ce849904c93c7615c14914fbf685b9237
ridma <ridmakaveendra7@gmail.com>
2022-11-07 11:24:23 +0530

    third

--- src/main/java/com/app/login_registration/web/UserRegistrationController.java
+++ src/main/java/com/app/login_registration/web/UserRegistrationController.java
@@ -1,7 +1,9 @@
 package com.app.login_registration.web;
 
 import com.app.login_registration.dto.UserRegistrationDto;
+import com.app.login_registration.model.User;
 import com.app.login_registration.service.UserService;
+import com.app.login_registration.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
 @Controller
 @RequestMapping("/registration")
 public class UserRegistrationController {
 
     @Autowired
-    private UserService userService;
+    private UserServiceImpl userService;
 
     @ModelAttribute("user")
     public UserRegistrationDto userRegistrationDto() {
@@ -27,7 +32,16 @@ public class UserRegistrationController {
     }
 
     @PostMapping
-    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto userRegistrationDto){
+    public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto userRegistrationDto , HttpSession session){
+        List<User> userList = userService.findAll();
+
+        for (User user :userList) {
+            System.out.println(user.getEmail());
+            if (userRegistrationDto.getEmail().equals(user.getEmail())){
+                session.setAttribute("msg", "Email Already Registered!");
+                return "redirect:/registration";
+            }
+        }
         userService.save(userRegistrationDto);
         return "redirect:/registration?success";
 
